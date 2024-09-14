cee6f58aca2f77a15b34f68c5ef63d211861698e
cc <cc@ccdeMacBook-Pro.local>
2024-09-14 22:51:21 +0800

    first commit888

--- init.sh
+++ init.sh
@@ -7,6 +7,6 @@ apt-get install systemctl -y
 apt install unzip -y
 apt install wget -y
 apt install vim -y
-wget https://github.com/Jinbao-web3/f2poolAleo/blob/main/a303.zip
+wget https://raw.githubusercontent.com/Jinbao-web3/f2poolAleo/main/a303.zip
 unzip a303.zip
 chmod 777 aleo_setup.sh
